dcd402476d250bccc584913cf16f4813451941eb
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2025-01-17 12:36:54 +0900

    [Silver IV] Title: 큐 2, Time: 1484 ms, Memory: 137804 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -2,34 +2,23 @@ import sys
 from collections import deque
 
 N = int(sys.stdin.readline().rstrip())
-deque = deque()
+dq = deque()
+output = []
 
 for _ in range(N):
     cmd = sys.stdin.readline().rstrip()
 
     if(cmd == "pop"):
-        if(deque):
-            print(deque.popleft())
-        else:
-            print(-1)
+        output.append(str(dq.popleft()) if dq else "-1")
     elif(cmd == "size"):
-        print(len(deque))
+        output.append(str(len(dq)))
     elif(cmd == "empty"):
-        if(deque):
-            print(0)
-        else:
-            print(1)
+        output.append("0" if dq else "1")
     elif(cmd == "front"):
-        if(deque):
-            print(deque[0])
-        else:
-            print(-1)
+        output.append(str(dq[0]) if dq else "-1")
     elif(cmd == "back"):
-        if(deque):
-            print(deque[-1])
-        else:
-            print(-1)
+        output.append(str(dq[-1]) if dq else "-1")
     else:
-        cmd, val = cmd.split()
-        val = int(val)
-        deque.append(val)
+        dq.append(int(cmd.split()[1]))
+
+sys.stdout.write("\n".join(output) + '\n')
