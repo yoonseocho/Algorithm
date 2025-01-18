96b612cc6d82026aa6ce91cb12bfe308e2edf3f2
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2025-01-18 22:20:43 +0900

    [Silver IV] Title: 덱 2, Time: 728 ms, Memory: 102548 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.py
+++ 백준/Silver/28279. 덱 2/덱 2.py
@@ -4,24 +4,25 @@ import sys
 dq = deque()
 output = []
 
-for _ in range(int(sys.stdin.readline().rstrip())):
-    cmd = sys.stdin.readline().rstrip()
-    if(cmd == "3"):
+input = sys.stdin.readline
+
+for _ in range(int(input().strip())):
+    cmd = input().strip().split()
+    if(cmd[0] == "1"):
+        dq.appendleft(int(cmd[1]))
+    elif(cmd[0] == "2"):
+        dq.append(int(cmd[1]))
+    elif(cmd[0] == "3"):
         output.append(dq.popleft() if dq else -1)
-    elif(cmd == "4"):
+    elif(cmd[0] == "4"):
         output.append(dq.pop() if dq else -1)
-    elif(cmd == "5"):
+    elif(cmd[0] == "5"):
         output.append(len(dq))
-    elif(cmd == "6"):
+    elif(cmd[0] == "6"):
         output.append(1 if len(dq) == 0 else 0)
-    elif(cmd == "7"):
+    elif(cmd[0] == "7"):
         output.append(dq[0] if dq else -1)
-    elif(cmd == "8"):
+    elif(cmd[0] == "8"):
         output.append(dq[-1] if dq else -1)
-    else:
-        if(cmd.split()[0] == "1"):
-            dq.appendleft(int(cmd.split()[1]))
-        elif(cmd.split()[0] == "2"):
-            dq.append(int(cmd.split()[1]))
-    
+
 sys.stdout.write("\n".join(map(str, output))+"\n")
