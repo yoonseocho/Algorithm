d3918a76d71bc7418e69534c5e127371262dcb26
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2025-01-20 23:57:38 +0900

    [Silver II] Title: 스택 수열, Time: 104 ms, Memory: 35412 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+stack = []
+result = []
+current = 1
+
+for _ in range(int(input().rstrip())):
+    num = int(input().rstrip())
+    while current <= num:
+        stack.append(current)
+        result.append('+')
+        current += 1
+    if stack[-1] == num:
+        stack.pop()
+        result.append('-')
+    else:
+        print("NO")
+        break
+else:
+    print('\n'.join(result)+'\n')
