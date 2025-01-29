56fcef72f15b494e1c1fcbb5913a700afd00a435
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2025-01-29 23:10:46 +0900

    [Silver III] Title: 후위 표기식2, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1935. 후위 표기식2/후위 표기식2.py
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.py
@@ -1,30 +1,32 @@
-N = int(input())
-postfix = input()
+import sys
+
+input = sys.stdin.read
+
+data = input().splitlines()
+
+N = int(data[0])
+postfix = data[1]
 
 stack = []
 nums = {}
 for i in range(N):
     char = chr(ord('A') + i)
-    nums[char] = int(input())
+    nums[char] = int(data[2+i])
 
 for p in postfix:
-    if p == "+":
+    if p in ['+', '-', '*', '/']:
         a = stack.pop()
         b = stack.pop()
-        stack.append(a + b)
-    elif p == "-":
-        a = stack.pop()
-        b = stack.pop()
-        stack.append(b - a)
-    elif p == "*":
-        a = stack.pop()
-        b = stack.pop()
-        stack.append(a * b)
-    elif p == "/":
-        a = stack.pop()
-        b = stack.pop()
-        stack.append(b / a)
+        if p == "+":
+            b += a
+        elif p == "-":
+            b -= a
+        elif p == "*":
+            b *= a
+        elif p == "/":
+            b /= a
+        stack.append(b)
     else:
         stack.append(nums[p])
     
-print(f"{stack[-1] :.2f}")
+print(f"{stack[0] :.2f}")
