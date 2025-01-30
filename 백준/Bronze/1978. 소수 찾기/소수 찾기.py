5ec4e8f68470bd53c33ae0f7c635c769af6e3cc3
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2025-01-30 23:15:52 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,11 +1,19 @@
+import math
+
+def is_prime(x):
+    if x < 2:
+        return False
+    for i in range(2, int(math.sqrt(x))+1):
+        if x % i == 0:
+            return False
+    return True
+
+
 N = int(input())
 nums = list(map(int, input().split()))
 count = 0
 
 for num in nums:
-    for i in range(2, num+1):
-        if num % i == 0:
-            if num == i:
-                count += 1
-            break
+    if is_prime(num):
+        count += 1
 print(count)
