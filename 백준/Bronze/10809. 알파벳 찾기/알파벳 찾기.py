4b6cdc322428585529dbaeacaaa069b4369bb1bf
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2025-01-30 19:31:12 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,9 +1,4 @@
 word = input()
 
-alphabet = [-1] * 26
-
-for idx, val in enumerate(word):
-    if alphabet[ord(val)-ord('a')] == -1:
-        alphabet[ord(val)-ord('a')] = idx
-
-print(*alphabet)
+for i in range(ord('a'), ord('z')+1):
+    print(word.find(chr(i)), end = " ")
