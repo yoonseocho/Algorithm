47b92b06fedae65f123aca78a124b64c11e3142c
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2025-02-03 13:37:43 +0900

    [Silver II] Title: 숨바꼭질 6, Time: 68 ms, Memory: 46876 KB -BaekjoonHub

--- 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.py
+++ 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.py
@@ -1,9 +1,7 @@
 import math
-from functools import reduce
-
 def gcd_of_distances(subin, sisters):
     distances = [abs(subin - s) for s in sisters]
-    return reduce(math.gcd, distances)
+    return math.gcd(*distances)
 
 n, s = map(int, input().split())
 sisters = list(map(int, input().split()))
