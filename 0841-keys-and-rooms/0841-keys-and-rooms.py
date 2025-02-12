1129abd913d474ed95ab0626af28e02b85c68b6b
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2025-02-12 21:51:52 +0900

    Time: 0 ms (100%), Space: 18.1 MB (89.06%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        visited = [False] * len(rooms)
+        def dfs(v):
+            visited[v] = True
+            for next_v in rooms[v]:
+                if not visited[next_v]:
+                    dfs(next_v)
+        dfs(0)
+
+        return all(visited)
+
+        
