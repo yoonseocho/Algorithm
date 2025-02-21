664b556f2369d4b848523c6c47356731f55df665
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2025-02-21 21:52:05 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 136 ms, Memory: 44072 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -1,21 +1,12 @@
 from collections import deque
 
-def print_path(prev, target):
-    path = []
-    while target != -1:
-        path.append(target)
-        target = prev[target]
-    print(*path[::-1])
-
-
 def bfs(start, target):
     if start == target:
         print(0)
         print(start)
-        return 
-    
+        return
     visited = [-1] * 100001
-    prev = [-1] * 100001
+    prev_path = [-1] * 100001
     q = deque()
     q.append(start)
     visited[start] = 0
@@ -23,16 +14,26 @@ def bfs(start, target):
     while q:
         cur_pos = q.popleft()
 
-        for next_pos in (cur_pos - 1, cur_pos +1, cur_pos * 2):
+        for next_pos in (cur_pos - 1, cur_pos + 1, cur_pos * 2):
             if 0 <= next_pos <= 100000 and visited[next_pos] == -1:
-                prev[next_pos] = cur_pos
-                visited[next_pos] = visited[cur_pos] + 1
                 q.append(next_pos)
-            
+                visited[next_pos] = visited[cur_pos] + 1
+                prev_path[next_pos] = cur_pos
+
             if next_pos == target:
-                print(visited[next_pos])
-                print_path(prev, target)
+                print(visited[next_pos]) 
+                print_path(prev_path, target)
                 return
+            
+def print_path(prev_path, target):
+    output = []
+
+    while target != -1:
+        output.append(target)
+        target = prev_path[target]
+    print(*output[::-1])
+
+
 
 n, k = map(int, input().split())
 bfs(n, k)
