661aa0b5b6319300455aecbdfe85ac20e0e0eb31
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2025-03-06 00:02:02 +0900

    [level 3] Title: 베스트앨범, Time: 0.15 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,20 +1,20 @@
 def solution(genres, plays):
     memo = {}
-    
     for idx, val in enumerate(genres):
         memo[(idx, val)] = plays[idx]
-    
-    genre_play_count = {}
+        
+    genre_count = {}
     for (idx, genre), play in memo.items():
-        if genre not in genre_play_count:
-            genre_play_count[genre] = 0
-        genre_play_count[genre] += play
+        if genre not in genre_count:
+            genre_count[genre] = 0
+        genre_count[genre] += play
     
-    sorted_genres = sorted(genre_play_count.keys(), key=lambda g: genre_play_count[g], reverse=True)
+    sorted_genres = sorted(genre_count.keys(), key=lambda g: genre_count[g], reverse=True)
     
     result = []
     for genre in sorted_genres:
         songs = [(idx, play) for (idx, g), play in memo.items() if g == genre]
-        top_songs = sorted(songs, key=lambda x: (-x[1], x[0]))[:2]
-        result.extend(idx for idx, _ in top_songs)
+        songs.sort(key=lambda x: (-x[1], x[0]))
+        result.extend(idx for idx, _ in songs[:2])
+        
     return result
