e7110d6ac8659384abac67776d76b93b93b02f37
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2025-04-10 20:02:02 +0900

    [Silver I] Title: 스타트와 링크, Time: 1736 ms, Memory: 57548 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -0,0 +1,33 @@
+import sys
+from itertools import combinations
+
+input = sys.stdin.readline
+
+
+def solution():
+    global min_diff
+    
+    comb = list(combinations(member_list, n//2))
+
+    for team in comb:
+        start_team = list(team)
+        link_team = list(set(member_list) - set(start_team))
+
+        start_score = 0
+        link_score = 0
+
+        for i, j in combinations(start_team, 2):
+            start_score += s[i][j] + s[j][i]
+        for i, j in combinations(link_team, 2):
+            link_score += s[i][j] + s[j][i]
+
+        min_diff = min(min_diff, abs(start_score - link_score))
+    return min_diff
+
+n= int(input())
+s = [list(map(int, input().split())) for _ in range(n)]
+
+member_list = [i for i in range(n)]
+min_diff = float('inf')
+
+print(solution())
