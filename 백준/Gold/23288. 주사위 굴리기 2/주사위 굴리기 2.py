cdaf6541c049aa4cbde29b624e3b55359c6d0ca4
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2025-04-11 13:22:31 +0900

    [Gold III] Title: 주사위 굴리기 2, Time: 324 ms, Memory: 35100 KB -BaekjoonHub

--- 백준/Gold/23288. 주사위 굴리기 2/주사위 굴리기 2.py
+++ 백준/Gold/23288. 주사위 굴리기 2/주사위 굴리기 2.py
@@ -3,11 +3,42 @@ from collections import deque
 
 input = sys.stdin.readline
 
-# 동, 남, 서, 북 (시계 방향)
+# 시계 방향 (동, 남, 서, 북)
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
-def get_score(x, y): # 주사위가 도착한 칸 (x, y)에 대한 점수를 획득
+def roll_dice(direction):
+    global dice
+
+    top = dice[1]
+    north = dice[2]
+    east = dice[3]
+    west = dice[4]
+    south = dice[5]
+    bottom = dice[6]
+
+    if direction == 1:
+        dice[1] = west
+        dice[3] = top
+        dice[4] = bottom
+        dice[6] = east
+    elif direction == 2:
+        dice[1] = north
+        dice[2] = bottom
+        dice[5] = top
+        dice[6] = south
+    elif direction == 3:
+        dice[1] = east
+        dice[3] = bottom
+        dice[4] = top
+        dice[6] = west
+    elif direction == 4:
+        dice[1] = south
+        dice[2] = top
+        dice[5] = bottom
+        dice[6] = north
+
+def get_score(x, y):
     visited = [[False] * m for _ in range(n)]
     q = deque()
     q.append((x, y))
@@ -16,99 +47,54 @@ def get_score(x, y): # 주사위가 도착한 칸 (x, y)에 대한 점수를 획
     count = 1
 
     while q:
-        x, y = q.popleft()
-        for d in range(4):
-            nx = x + dx[d]
-            ny = y + dy[d]
+        cx, cy = q.popleft()
 
-            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and board[nx][ny] == num:
+        for i in range(4):
+            nx = cx + dx[i]
+            ny = cy + dy[i]
+
+            if (0 <= nx < n and 0 <= ny < m) and not visited[nx][ny] and board[nx][ny] == num:
                 visited[nx][ny] = True
                 q.append((nx, ny))
                 count += 1
-
     return count * num
 
+def solution():
+    global x, y
 
-def roll_dice(direction):
-    temp = dice[:]
-
-    top = temp[1]
-    north = temp[2]
-    east = temp[3]
-    west = temp[4]
-    south = temp[5]
-    bottom = temp[6]
-
-    if direction == 0: # 동
-        dice[1] = west
-        dice[3] = top
-        dice[4] = bottom
-        dice[6] = east
-    elif direction == 1: # 남
-        dice[1] = north
-        dice[2] = bottom
-        dice[5] = top
-        dice[6] = south
-    elif direction == 2: # 서
-        dice[1] = east
-        dice[3] = bottom
-        dice[4] = top
-        dice[6] = west
-    elif direction == 3: # 븍
-        dice[1] = south
-        dice[2] = top
-        dice[5] = bottom
-        dice[6] = north
-
-
-def get_bottom_of_dice():
-    return dice[6]
-
-def get_direction_of_dice(a, b, direction): # 주사위 이동 방향
-    if a > b: # 90도 시계 방향 회전
-        return (direction + 1) % 4
-    elif a < b: # 90도 반시계 방향 회전
-        return (direction + 3) % 4
-    else: # 변화 X 그대로
-        return direction
-
-def simulation():
-    global dice
-
-    x, y = 0, 0
     direction = 0
     total_score = 0
 
-    for _ in range(k):
+    for i in range(k):
         nx = x + dx[direction]
         ny = y + dy[direction]
 
-        if not (0 <= nx < n and 0 <= ny < m):
+        if not (0<= nx < n and 0<= ny < m):
             direction = (direction + 2) % 4
             nx = x + dx[direction]
             ny = y + dy[direction]
         
-        roll_dice(direction)
-
         x = nx
         y = ny
 
+        roll_dice(direction+1)
+
         total_score += get_score(x, y)
-        
-        a = get_bottom_of_dice()
-        b = board[x][y]
-        direction = get_direction_of_dice(a, b, direction)
+
+        A = dice[6]
+        B = board[x][y]
+        if A > B:
+            direction = (direction + 1) % 4
+        elif A < B:
+            direction = (direction + 3) % 4
 
     return total_score
 
 
+
 n, m, k = map(int, input().split())
-board = []
-for _ in range(n):
-    line = map(int, input().split())
-    row = list(line)
-    board.append(row)
+board = [list(map(int, input().split())) for _ in range(n)]
+x = y = 0
 dice = [0, 1, 2, 3, 4, 5, 6]
 
-print(simulation())
-    
+print(solution())
