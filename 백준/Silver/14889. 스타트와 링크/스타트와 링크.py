e821a6afa6835411b79d08a771cc11d1f8cbac81
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2025-04-12 17:20:25 +0900

    [Silver I] Title: 스타트와 링크, Time: 1616 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,8 +1,4 @@
-import sys
-
-input = sys.stdin.readline
-
-def divide_team():
+def devide_team(visited):
     global min_diff
     
     start_score = 0
@@ -14,35 +10,42 @@ def divide_team():
                 start_score += s[i][j] + s[j][i]
             elif not visited[i] and not visited[j]:
                 link_score += s[i][j] + s[j][i]
-    
+
     diff = abs(start_score - link_score)
-    min_diff = min(min_diff, diff)
 
-    if min_diff == 0:
+    if diff == 0:
         print(0)
-        sys.exit()
+        exit()
 
+    min_diff = min(min_diff, diff)
+    
+    
 
 def dfs(idx, current_people):
+    global visited
+
     if current_people == n // 2:
-        divide_team()
+        devide_team(visited)
         return
     
     for i in range(idx, n):
         if not visited[i]:
+            current_people += 1
             visited[i] = True
-            dfs(i + 1, current_people + 1)
+            dfs(i + 1, current_people)
             visited[i] = False
+            current_people -= 1
 
-n= int(input())
+    
+n = int(input())
 s = [list(map(int, input().split())) for _ in range(n)]
-
 member_list = [i for i in range(n)]
+
 min_diff = float('inf')
+min_list = []
 
 visited = [False] * n
-
-
 visited[0] = True
 dfs(1, 1)
+
 print(min_diff)
