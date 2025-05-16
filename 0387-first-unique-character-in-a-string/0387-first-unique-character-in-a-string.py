54eff99c93f7e8afdbd03f237b12c08e3cd8c760
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2025-05-16 13:00:19 +0900

    Time: 54 ms (68.65%), Space: 18 MB (55.02%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,13 +1,10 @@
+from collections import Counter
+
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        memo = {}
-        for char in s:
-            if char not in memo:
-                memo[char] = 1
-            else:
-                memo[char] += 1
+        count = Counter(s)
         
         for idx, char in enumerate(s):
-            if memo[char] == 1:
+            if count[char] == 1:
                 return idx
         return -1
