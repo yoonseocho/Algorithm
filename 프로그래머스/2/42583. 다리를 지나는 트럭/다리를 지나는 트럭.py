b6e109976152233b8af699f9ac1fd4ec36637a16
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-01-30 23:32:22 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 55.59 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,62 +1,27 @@
 from collections import deque
 
-def load_new_truck(on_bridge, q):
-    truck_weight, each_time = q.popleft()
-    each_time += 1
-    on_bridge.append([truck_weight, each_time])
-    
-def handle_exist_truck(on_bridge):
-    for each_truck in on_bridge:
-        each_truck[1] += 1
 
-def can_go(on_bridge, q, weight, bridge_length):
-    total_weight = sum(each_truck[0] for each_truck in on_bridge)
-    return total_weight + q[0][0] <= weight and len(on_bridge) + 1 <= bridge_length
-
-
-def can_finish_bridge(on_bridge, bridge_length):
-    return on_bridge[0][1] == bridge_length
-
-def init(truck_weights):
-    q = deque()
-    for truck_weight in truck_weights:
-        q.append([truck_weight, 0])
-    
-    time = 0
-    on_bridge = deque()
-    off_bridge = deque()
-    
-    load_new_truck(on_bridge, q)
-    time += 1
-    
-    return q, on_bridge, off_bridge, time
-    
 def solution(bridge_length, weight, truck_weights):
+    time = 0
+    bridge = deque([0]*bridge_length)
+    truck_weights = deque(truck_weights)
     
-    q, on_bridge, off_bridge, time = init(truck_weights)
+    current_weight = 0
     
-    while on_bridge:
-        # 1. 다리 탈출
-        if can_finish_bridge(on_bridge, bridge_length):
-            truck = on_bridge.popleft()
-            off_bridge.append(truck)
+    while bridge:
+        time += 1
+        # 1. 다리에서 트럭 (혹은 빈 공간 0)이 나감
+        exited = bridge.popleft()
+        current_weight -= exited
         
-        if q:
-            # 2. 다리 위에 못 올라감
-            if not can_go(on_bridge, q, weight, bridge_length):
-                handle_exist_truck(on_bridge)
-                time += 1
-                continue
-
-            # 3. 다리 위에 올라감
-            # 3-1. 기존 다리 위에 있는 시간 더해주고
-            handle_exist_truck(on_bridge)
-            # 3-2. 새로운 트럭 투입
-            load_new_truck(on_bridge, q)
-            time += 1
-        else:
-            handle_exist_truck(on_bridge)
-            time += 1
-        
+        # 2. 다음 트럭이 다리에 올라올 수 있는지 확인
+        if truck_weights:
+            if truck_weights[0] + current_weight <= weight:
+                new_truck = truck_weights.popleft()
+                bridge.append(new_truck)
+                current_weight += new_truck
+            else:
+                bridge.append(0)
+    
     return time
-        
+    
