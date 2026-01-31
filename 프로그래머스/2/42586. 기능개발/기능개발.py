411c5db4bf2d41ba4784c1b88bf3469841f32010
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-01-31 14:05:13 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,21 +1,16 @@
-import math
-
 def solution(progresses, speeds):
     days = []
     for progress, speed in zip(progresses, speeds):
-        day = math.ceil((100-progress)/speed)
+        day = -((progress-100)//speed)
         days.append(day)
     
-    answer = []
-    max_num = -1
-    cnt = 0
+    release = []
+    max_num = 0
     for day in days:
         if day > max_num:
             max_num = day
-            if cnt != 0:
-                answer.append(cnt)
-            cnt = 1
+            release.append(1)
         else:   
-            cnt += 1
-    answer.append(cnt)
-    return answer
+            release[-1] += 1
+    
+    return release
