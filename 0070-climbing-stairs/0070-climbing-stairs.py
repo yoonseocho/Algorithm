33030b579cc8d3b63a78cce5ba6a68a81eceb8c0
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-02-06 10:52:41 +0900

    Time: 0 ms (100%), Space: 19.4 MB (30.35%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,10 +1,12 @@
+from itertools import permutations
+
 class Solution:
-    memo = {1:1, 2:2}
+    memo = {}
     def climbStairs(self, n: int) -> int:
         if n == 1:
             return 1
         if n == 2:
             return 2
-        for i in range(3, n+1):
-            self.memo[i] = self.memo[i-1] + self.memo[i-2]
+        if n not in self.memo:
+            self.memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
         return self.memo[n]
