743aacac88418380372d4bb2572443e90d24f66a
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-02-06 10:34:02 +0900

    Time: 0 ms (100%), Space: 19.4 MB (30.35%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,15 +1,12 @@
-from collections import deque
-
-memo = {}
+from itertools import permutations
 
 class Solution:
+    memo = {}
     def climbStairs(self, n: int) -> int:
         if n == 1:
             return 1
-        elif n == 2:
+        if n == 2:
             return 2
-        
-        if n not in memo:
-            memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
-        return memo[n]
-            
+        if n not in self.memo:
+            self.memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        return self.memo[n]
