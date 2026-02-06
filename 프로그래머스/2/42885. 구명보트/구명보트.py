5f9e0b35367c5180cb18553fa51db01bbd9c731f
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-02-06 16:20:49 +0900

    [level 2] Title: 구명보트, Time: 7.73 ms, Memory: 9.46 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,16 +1,12 @@
 def solution(people, limit):
     people.sort()
-    n = len(people)
-    start, end = 0, n-1
+    start, end = 0, len(people)-1
     cnt = 0
-    while start <= end:
-        if people[start] + people[end] > limit:
-            end -= 1
+    while start < end:
+        if people[start] + people[end] <= limit:
+            start += 1
             cnt += 1
-            continue
-        cnt += 1
-        start += 1
         end -= 1
-    return cnt
+    return len(people) - cnt
         
         
