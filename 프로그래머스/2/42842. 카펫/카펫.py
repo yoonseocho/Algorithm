9defe317520fd54c1e03a23d93b722a883c25d96
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-02-06 11:22:37 +0900

    [level 2] Title: 카펫, Time: 564.29 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,5 +1,11 @@
 def solution(brown, yellow):
-    for h in range(3, brown):
-        for w in range(h, brown):
-            if 2*w + 2*h - 4 == brown and (w-2)*(h-2) == yellow:
-                return [w, h]
+    total = brown + yellow
+    candidates = []
+    for h in range(3, brown//2):
+        for w in range(3, brown//2):
+            if (h-2)*(w-2) == yellow:
+                candidates.append((h, w))
+    
+    for h, w in candidates:
+        if h*w - (h-2)*(w-2) == brown:
+            return [w, h]
