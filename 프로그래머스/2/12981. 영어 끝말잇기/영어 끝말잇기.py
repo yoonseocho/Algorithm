f4ec4974d9a2864c01ee682306c554d9765aaad2
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-02-13 00:10:24 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 9.04 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,14 +1,12 @@
-memo = {}
-
 def solution(n, words):
-    last = words[0][-1]
-    memo[words[0]] = 1
-    for idx, word in enumerate(words[1:], start=1):
+    memo = set()
+    prev_word = words[0][0]
+    for idx, word in enumerate(words):
         order = (idx % n) + 1
         turn = idx // n + 1
-        if word in memo or len(word) == 1 or last != word[0]:
+        if word in memo or prev_word != word[0]:
             return [order, turn]
-        memo[word] = order
-        last = word[-1]
+        memo.add(word)
+        prev_word = word[-1]
     return [0, 0]
         
