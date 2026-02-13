59c71c30b498f50fbebb61b0a0a59f6ca70cdffd
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-02-13 18:59:05 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,10 +1,8 @@
 def solution(n,a,b):
-    round = 1
+    round = 0
     
-    while a*b != 2:
-        if (a < b and a % 2 != 0 and b % 2 == 0 and abs(a-b) == 1) or (a > b and b % 2 != 0 and a % 2 == 0 and abs(a-b) == 1):
-            break
-        a = -((-a)//2)
-        b = -((-b)//2)
+    while a != b:
+        a = (a+1)//2
+        b = (b+1)//2
         round += 1
     return round
