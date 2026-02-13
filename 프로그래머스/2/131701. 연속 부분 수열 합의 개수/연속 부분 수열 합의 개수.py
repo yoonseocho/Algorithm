c808a636d9e00314ea1694562e37952a0f09f050
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-02-13 17:03:58 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 250.65 ms, Memory: 42.8 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -3,8 +3,7 @@ def solution(elements):
     memo = set()
     
     for i in range(n):
-        ssum = 0
-        ssum += elements[i]
+        ssum = elements[i]
         memo.add(ssum)
         for j in range(i+1, i+n):
             ssum += elements[j%n]
