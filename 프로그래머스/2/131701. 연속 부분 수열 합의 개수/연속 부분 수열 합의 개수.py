d1c3946d38a2096dd5df44b66149a014c1f5506c
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-02-13 17:03:21 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 238.10 ms, Memory: 42.7 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,19 +1,12 @@
-from collections import deque
-
 def solution(elements):
-    q = deque(elements)
+    n = len(elements)
     memo = set()
     
-    for _ in range(len(q)):
-        start = 0
-        rear = len(q)-1
-        tmp_sum = sum(q)
-        memo.add(tmp_sum)
-        while start != rear:
-            tmp_sum -= q[rear]
-            rear -= 1
-            if tmp_sum not in memo:
-                memo.add(tmp_sum)
-        curr_element = q.popleft()
-        q.append(curr_element)
+    for i in range(n):
+        ssum = 0
+        ssum += elements[i]
+        memo.add(ssum)
+        for j in range(i+1, i+n):
+            ssum += elements[j%n]
+            memo.add(ssum)
     return len(memo)
