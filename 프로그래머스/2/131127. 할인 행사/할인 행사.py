9592d7d8834c0565a5dc6a774a5fa8aa46adadc1
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-02-14 11:18:31 +0900

    [level 2] Title: 할인 행사, Time: 151.36 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,18 +1,10 @@
-def is_same(memo, want, number):
-    for key, value in zip(want, number):
-        if key not in memo or memo[key] != value:
-            return False
-    return True
+from collections import Counter
 
 def solution(want, number, discount):
     possible = 0
+    target = dict(zip(want, number))
     for i in range(len(discount)-9):
-        memo = {}
-        for j in range(i, i+10):
-            if discount[j] not in memo:
-                memo[discount[j]] = 1
-                continue
-            memo[discount[j]] += 1
-        if is_same(memo, want, number):
+        memo = Counter(discount[i:i+10])
+        if memo == target:
             possible += 1
     return possible
