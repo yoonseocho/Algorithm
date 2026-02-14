5c4a50af6432704430cd48c3c3801c6c0a26db94
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-02-14 11:31:33 +0900

    [level 2] Title: 할인 행사, Time: 75.33 ms, Memory: 14.7 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -3,8 +3,21 @@ from collections import Counter
 def solution(want, number, discount):
     possible = 0
     target = dict(zip(want, number))
-    for i in range(len(discount)-9):
-        memo = Counter(discount[i:i+10])
+    memo = Counter(discount[:10])
+    
+    if memo == target:
+        possible += 1
+        
+    for i in range(1, len(discount)-9):
+        prev_item = discount[i-1]
+        next_item = discount[i+9]
+        
+        memo[prev_item] -= 1
+        if memo[prev_item] == 0:
+            del memo[prev_item]
+        
+        memo[next_item] += 1
+        
         if memo == target:
             possible += 1
     return possible
