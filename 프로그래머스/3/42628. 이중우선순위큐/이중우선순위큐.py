67699486a2799ec656fe8094b336de897d5eac22
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-02-14 12:30:48 +0900

    [level 3] Title: 이중우선순위큐, Time: 226.48 ms, Memory: 15.5 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,45 +1,15 @@
-from collections import deque
-
-def remove_max(q):
-    if not q:
-        return
-    
-    max_val = float('-inf')
-    for element in q:
-        max_val = max(element, max_val)
-
-    while True:
-        if q[0] == max_val:
-            q.popleft()
-            break
-        else:
-            q.append(q.popleft())
-
-def remove_min(q):
-    if not q:
-        return
-    
-    min_val = float('inf')
-    for element in q:
-        min_val = min(element, min_val)
-
-    while True:
-        if q[0] == min_val:
-            q.popleft()
-            break
-        else:
-            q.append(q.popleft())
-
 def solution(operations):
-    q = deque()
+    q = []
     
     for operation in operations:
         cmd, data = operation.split()
         if cmd == "I":
             q.append(int(data))
-        elif cmd == "D" and data == "1":
-            remove_max(q)
-        elif cmd == "D" and data == "-1":
-            remove_min(q)
+        elif q and data == "1":
+            q.sort()
+            q.pop()
+        elif q and data == "-1":
+            q.sort(reverse=True)
+            q.pop()
 
     return [max(q), min(q)] if q else [0, 0]
