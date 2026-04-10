4f9fc6fb626575a1b58328fecf3d112001300683
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-04-11 00:18:49 +0900

    [Silver V] Title: 백대열, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/14490. 백대열/백대열.py
+++ 백준/Silver/14490. 백대열/백대열.py
@@ -1,16 +1,10 @@
 import sys
+import math
 
 input = sys.stdin.readline
 
 n,m = map(int, input().strip().split(":"))
 
-def gcd(n, m):
-    tmp = 1
-    for i in range(2, min(n, m)+1):
-        if n % i == 0 and m % i == 0:
-            tmp = i
-    return tmp
-
-num = gcd(n, m)
+num = math.gcd(n, m)
 
 print(f"{n//num}:{m//num}")
