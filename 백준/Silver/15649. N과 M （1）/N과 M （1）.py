fc7c6429aa041ffc36d7c2e807c13c77df7d4e29
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-04-17 20:27:38 +0900

    [Silver III] Title: N과 M (1), Time: 156 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,9 +1,23 @@
-from itertools import permutations
-
 n, m = map(int, input().split())
 
-nums = [num for num in range(1, n+1)]
-cases = permutations(nums, m)
+path = []
+visited = [False] * (n+1)
+def backtracking(depth):
+    #print(f"ENTER: depth={depth}, path={path}")
+    if depth == m:
+        print(*path)
+        #print(" >>> 완료! 저장: {path}")
+        return
+    
+    for i in range(1, n+1):
+        if not visited[i]:
+            visited[i] = True
+            path.append(i)
 
-for p in cases:
-    print(*p)
+            backtracking(depth+1)
+
+            path.pop()
+            visited[i] = False
+            #print(f"BACK: depth={depth}, path={path}")
+
+backtracking(0)
