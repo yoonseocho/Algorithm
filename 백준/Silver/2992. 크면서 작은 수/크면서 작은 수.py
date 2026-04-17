c5b2d3932ebcdbbc29b8aca4fd31328c2af3ca4e
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-04-17 19:23:31 +0900

    [Silver III] Title: 크면서 작은 수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.py
+++ 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.py
@@ -1,13 +1,37 @@
-from itertools import permutations
+import sys
 
-X_str = input().strip()
+X_str = sys.stdin.readline().strip()
 X_int = int(X_str)
+n = len(X_str)
 
-cases = sorted({int(''.join(p)) for p in permutations(X_str)})
+digit = list(X_str)
+cases = []
+path = []
+visited = [False] * n
+def backtracking(depth):
+    #print(f"ENTER: depth={depth}, path={path}")
+    if depth == n:
+        cases.append(int(''.join(path)))
+        #print(f" >>> 완성! 저장됨: {path}")
+        return
+    
+    for i in range(n):
+        if not visited[i]:
+            visited[i] = True
+            path.append(digit[i])
 
+            backtracking(depth+1)
+
+            path.pop()
+            visited[i] = False
+            #print(f"BACK: depth={depth}, path={path}")
+
+backtracking(0)
+
+sorted_nums = sorted(set(cases))
 result = 0
-for n in cases:
-    if n > X_int:
-        result = n
+for case in sorted_nums:
+    if case > X_int:
+        result = case
         break
 print(result)
