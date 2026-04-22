904945ca7243e8c52623a142e091ad2686fdb59e
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-04-22 13:06:27 +0900

    [level 2] Title: 프로세스, Time: 0.55 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -2,16 +2,20 @@ from collections import deque
 
 def solution(priorities, location):
     q = deque()
-    cnt = 0
     
-    for idx, val in enumerate(priorities):
-        q.append((idx, val))
+    for idx, p in enumerate(priorities):
+        q.append((idx, p))
     
+    max_num = max(priorities)
+    result = []
     while q:
-        if q[0][1] == max(q, key=lambda x: x[1])[1]:
-            if q[0][0] == location:
-                return cnt + 1
-            q.popleft()
-            cnt += 1
+        each = q.popleft()
+        if each[1] < max_num:
+            q.append(each)
         else:
-            q.append(q.popleft())
+            result.append(each)
+            if q:
+                max_num = max(q, key = lambda x: x[1])[1]
+    for i, (idx, _) in enumerate(result):
+        if idx == location:
+            return i + 1
