9286cd61e606626aac19245122ea6b18eba07d77
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-04-28 23:29:10 +0900

    [level 2] Title: 소수 찾기, Time: 18.80 ms, Memory: 9.45 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,21 +1,26 @@
-from itertools import permutations
-
 def is_prime(num):
     if num == 0 or num == 1:
         return False
     
-    for i in range(2, num):
+    for i in range(2, int(num**0.5)+1):
         if num % i == 0:
             return False
     return True
     
 def solution(numbers):
     prime_num = set()
-    for i in range(1, len(numbers)+1):
-        cases = permutations(numbers, i)
+    visited = [False] * len(numbers)
     
-        for case in cases:
-            num = int(''.join(case))
+    def dfs(curr_str):
+        if curr_str:
+            num = int(curr_str)
             if is_prime(num):
                 prime_num.add(num)
+                
+        for i in range(len(numbers)):
+            if not visited[i]:
+                visited[i] = True
+                dfs(curr_str+numbers[i])
+                visited[i] = False
+    dfs("")
     return len(prime_num)
