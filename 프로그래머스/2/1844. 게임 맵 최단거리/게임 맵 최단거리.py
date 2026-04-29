196f1b8129e22e0eb667e4b31b30b83d963d2dac
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-04-30 00:11:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 14.58 ms, Memory: 9.48 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,27 +1,39 @@
 from collections import deque
 
-def bfs(maps):
-    delta = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-    
-    row = len(maps)
-    col = len(maps[0])
-    
-    q = deque()
-    q.append((0, 0))
-    
-    while q:
-        x, y = q.popleft()
-    
-        for dx, dy in delta:
-            next_x = x + dx
-            next_y = y + dy
-            
-            if 0 <= next_x < row and 0 <= next_y < col:
-                if maps[next_x][next_y] == 1:
-                    maps[next_x][next_y] = maps[x][y] + 1
-                    q.append((next_x, next_y))
-    
-    return maps[row - 1][col - 1] if maps[row - 1][col - 1] > 1 else -1
-
 def solution(maps):
-    return bfs(maps)
+    n = len(maps)
+    m = len(maps[0])
+    
+    dxs = [-1, 1, 0, 0]
+    dys = [0, 0, -1, 1]
+    
+    dist = [[0] * m for _ in range(n)]
+    q = deque()
+    
+    def in_range(x, y):
+        return 0<=x<n and 0<=y<m
+    
+    def bfs(x, y):
+        # if maps[n-1][m-2] == 0 and maps[n-2][m-1] == 0:
+        #     return -1
+        
+        q.append((x, y))
+        dist[x][y] = 1
+        
+        while q:
+            x, y = q.popleft()
+            
+            if x == n-1 and y == m-1:
+                return dist[n-1][m-1]
+            
+            for dx, dy in zip(dxs, dys):
+                nx, ny = x + dx, y + dy
+                if in_range(nx, ny) and maps[nx][ny] == 1 and dist[nx][ny] == 0:
+                    dist[nx][ny] = dist[x][y] + 1
+                    q.append((nx, ny))
+        return -1
+    
+    return bfs(0, 0)
+    
+                
+        
