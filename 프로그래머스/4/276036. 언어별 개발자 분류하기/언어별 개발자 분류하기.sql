455ee1d5197d5d03cf86717613d4d5c5960ed70b
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-04-30 23:16:51 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,19 +1,13 @@
 -- 코드를 작성해주세요
-SELECT 
+SELECT
     CASE
-        WHEN SKILL_CODE & (SELECT BIT_OR(CODE)
-                             FROM SKILLCODES
-                             WHERE CATEGORY = 'Front End') > 0
-                             AND SKILL_CODE & (SELECT BIT_OR(CODE) 
-                                                       FROM SKILLCODES 
-                                                       WHERE NAME = 'Python') > 0 THEN 'A'
-        WHEN SKILL_CODE & (SELECT BIT_OR(CODE)
-                            FROM SKILLCODES
-                            WHERE NAME = 'C#') > 0 THEN 'B'
-        WHEN SKILL_CODE & (SELECT BIT_OR(CODE)
-                         FROM SKILLCODES
-                         WHERE CATEGORY = 'Front End') > 0 THEN 'C'
-    END AS GRADE, ID, EMAIL
+        WHEN SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE CATEGORY = "Front End") 
+            AND SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = "Python") THEN 'A'
+        WHEN SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = "C#") THEN 'B'
+        WHEN SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE CATEGORY = "Front End") THEN 'C'
+    END AS GRADE,
+    ID,
+    EMAIL
 FROM DEVELOPERS
 HAVING GRADE IS NOT NULL
 ORDER BY GRADE, ID ASC;
