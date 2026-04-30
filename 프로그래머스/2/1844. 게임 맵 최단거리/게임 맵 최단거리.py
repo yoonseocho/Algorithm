bef6ec73818c4c59cde095409e9ba3bf0161b2f2
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-04-30 18:51:40 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 14.35 ms, Memory: 9.48 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,22 +1,18 @@
 from collections import deque
 
 def solution(maps):
-    n = len(maps)
-    m = len(maps[0])
+    n, m = len(maps), len(maps[0])
+    
+    dist = [[0]*m for _ in range(n)]
+    q = deque()
     
     dxs = [-1, 1, 0, 0]
     dys = [0, 0, -1, 1]
-    
-    dist = [[0] * m for _ in range(n)]
-    q = deque()
     
     def in_range(x, y):
         return 0<=x<n and 0<=y<m
     
     def bfs(x, y):
-        # if maps[n-1][m-2] == 0 and maps[n-2][m-1] == 0:
-        #     return -1
-        
         q.append((x, y))
         dist[x][y] = 1
         
@@ -24,16 +20,16 @@ def solution(maps):
             x, y = q.popleft()
             
             if x == n-1 and y == m-1:
-                return dist[n-1][m-1]
+                return dist[x][y]
             
             for dx, dy in zip(dxs, dys):
                 nx, ny = x + dx, y + dy
-                if in_range(nx, ny) and maps[nx][ny] == 1 and dist[nx][ny] == 0:
+                
+                if in_range(nx, ny) and not dist[nx][ny] and maps[nx][ny] == 1:
                     dist[nx][ny] = dist[x][y] + 1
                     q.append((nx, ny))
-        return -1
-    
-    return bfs(0, 0)
-    
-                
         
+        return -1
+                
+                
+    return bfs(0, 0)
