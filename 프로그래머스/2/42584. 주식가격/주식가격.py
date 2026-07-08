fad803504acbd5c437075daf6e30a2f9b1d7a2af
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-07-09 00:03:50 +0900

    [level 2] Title: 주식가격, Time: 17.82 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,17 +1,18 @@
 def solution(prices):
-    n = len(prices)
-    answer = [0] * n
+    ans = [0] * len(prices)
     stk = []
     
-    for curr_idx, price in enumerate(prices):
-        while stk and prices[stk[-1]] > price:
-            prev_idx = stk.pop()
-            answer[prev_idx] = curr_idx - prev_idx
-        stk.append(curr_idx)
+    for idx, price in enumerate(prices):
+        while stk and stk[-1][1] > price:
+            prev_idx, _ = stk.pop()
+            ans[prev_idx] = idx - prev_idx
+        stk.append((idx, price))
     
+    # 끝까지 안떨어진 것들
+    max_time = len(prices) - 1
     while stk:
-        prev_idx = stk.pop()
-        answer[prev_idx] = (n-1) - prev_idx
+        prev_idx, _ = stk.pop()
+        ans[prev_idx] = idx - prev_idx
+    return ans
     
-    return answer
-        
+    
