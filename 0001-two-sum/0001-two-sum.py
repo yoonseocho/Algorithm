33d8efaaf3ce342eeaf9723fed70b402a775a461
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-07-10 16:35:51 +0900

    Time: 0 ms (100%), Space: 20.6 MB (17.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_list = sorted(enumerate(nums), key=lambda x: x[1])
-        n = len(nums)
-        for i in range(n-1):
-            for j in range(n-1, i, -1):
-                two_sum = nums_list[i][1] + nums_list[j][1]
-                if two_sum == target:
-                    return [nums_list[i][0], nums_list[j][0]]
-                elif two_sum < target:
-                    break
+        num_dict = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in num_dict:
+                return [i, num_dict[complement]]
             
+            num_dict[num] = i
