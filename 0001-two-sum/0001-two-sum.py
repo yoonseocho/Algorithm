0f159259aa471407502bf74a2e070873eb4c4c63
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-07-10 16:29:11 +0900

    Time: 35 ms (35.08%), Space: 20.5 MB (40.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,21 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        answer = []
-        indent = "   "
-        def dfs(cur_sum, idx):
-            # print(f"{indent * idx}>>>dfs({cur_sum}, {idx})")
-            if len(answer) == 2:
-                if cur_sum == target:
-                    return True
-                return False
- 
-            for i in range(idx, len(nums)):
-                answer.append(i)
-                if dfs(cur_sum + nums[i], i+1):
-                    return True
-                # print(f"{indent * (i+1)}dfs함수 끝!")
-                answer.pop()
-
-        dfs(0, 0)
-        return answer
+        nums_list = sorted(enumerate(nums), key=lambda x: x[1])
+        n = len(nums)
+        for i in range(n-1):
+            for j in range(n-1, i, -1):
+                two_sum = nums_list[i][1] + nums_list[j][1]
+                if two_sum == target:
+                    return [nums_list[i][0], nums_list[j][0]]
+                elif two_sum < target:
+                    break
             
