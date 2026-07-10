cbfce516944938a1527737dbf1dc50540780a4c5
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-07-10 16:08:42 +0900

    Time: 6187 ms (5%), Space: 20.3 MB (58.52%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,21 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-        for idx, val in enumerate(nums):
-            complement = target - val
-            if complement in hashmap:
-                return [idx, hashmap[complement]]
-            hashmap[val] = idx
+        answer = []
+        indent = "   "
+        def dfs(cur_sum, idx):
+            # print(f"{indent * idx}>>>dfs({cur_sum}, {idx})")
+            if len(answer) == 2:
+                if cur_sum == target:
+                    return True
+                return False
+ 
+            for i in range(idx, len(nums)):
+                answer.append(i)
+                if dfs(cur_sum + nums[i], i+1):
+                    return True
+                # print(f"{indent * (i+1)}dfs함수 끝!")
+                answer.pop()
+
+        dfs(0, 0)
+        return answer
             
