bbad2b482564a8bf233f1c7a8e22fd4222f33a8c
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-07-15 23:13:02 +0900

    [level 3] Title: 단어 변환, Time: 0.12 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,36 +1,34 @@
 from collections import deque
 
 def solution(begin, target, words):
+    
     if target not in words:
         return 0
     
-    q = deque()
     visited = [False] * len(words)
     
     def is_one_letter_diff(word1, word2):
-        cnt = 0
-        
+        diff_cnt = 0
         for c1, c2 in zip(word1, word2):
             if c1 != c2:
-                cnt += 1
-        return True if cnt == 1 else False
-    
-    
-    def bfs(word, cnt):
-        q.append((word, cnt))
+                diff_cnt += 1
+            if diff_cnt > 1:
+                return False
         
+        return diff_cnt == 1
+            
+    def bfs():
+        q = deque([(begin, 0)])
+
         while q:
-            word, cnt = q.popleft()
+            curr_word, cnt = q.popleft()
             
-            if word == target:
+            if curr_word == target:
                 return cnt
-            
-            for idx, next_word in enumerate(words):
-                if is_one_letter_diff(word, next_word) and not visited[idx]:
-                    visited[idx] = True
-                    q.append((next_word, cnt+1))
-    
-    return bfs(begin, 0)
-    
-    
-    
+
+            for i, word in enumerate(words):
+                if is_one_letter_diff(curr_word, word) and not visited[i]:
+                    q.append((word, cnt+1))
+                    visited[i] = True
+                
+    return bfs()
