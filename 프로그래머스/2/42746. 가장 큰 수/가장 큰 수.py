55f78a60d8633fd0c7d0e871ef0ba294940965ff
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-07-18 16:38:22 +0900

    [level 2] Title: 가장 큰 수, Time: 31.83 ms, Memory: 27 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,6 +1,10 @@
-from itertools import permutations
-
 def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers.sort(key = lambda x: x*3, reverse = True)
-    return str(int(''.join(numbers)))
+    if sum(numbers) == 0:
+        return "0"
+    
+    numbers_str = list(map(str, numbers))
+    
+    numbers_str.sort(key=lambda x: x*3, reverse=True)
+    
+    return ''.join(numbers_str)
+    
