2b7d9ccac7e4aba3bbc152d076ff7ba640ffaac4
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-07-20 13:29:30 +0900

    [level 2] Title: H-Index, Time: 0.14 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,20 +1,3 @@
 def solution(citations):
-    n = len(citations)
-    h_index = 0
-    h_indexs = []
-    
-    while h_index <= n:
-        cnt1 = 0
-        
-        for citation in citations:
-            if citation >= h_index:
-                cnt1 += 1
-        
-        if cnt1 >= h_index:
-            h_indexs.append(h_index)
-        
-        h_index += 1
-    
-    return max(h_indexs) if h_indexs else 0
-            
-    
+    citations.sort(reverse=True)
+    return max(map(min, enumerate(citations, start=1)))
