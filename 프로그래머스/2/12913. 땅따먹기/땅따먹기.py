91919797996e878cf0d91c382f709c75e47238b9
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-07-22 16:44:29 +0900

    [level 2] Title: 땅따먹기, Time: 153.27 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -0,0 +1,8 @@
+def solution(land):
+    n = len(land)
+    
+    for i in range(1, n):
+        for j in range(4):
+            land[i][j] += max(land[i-1][:j]+land[i-1][j+1:])
+    return max(land[-1])
+            
