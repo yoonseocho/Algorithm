3eed955c91f6a9c4a21f17a1fa91983f3459fb4f
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-07-23 01:17:32 +0900

    Time: 479 ms (69.89%), Space: 19.5 MB (94.42%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,8 +1,6 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [float('inf')] * (amount + 1)
-        dp[0] = 0
-
+        dp = [0] + [float('inf')] * amount
         coins.sort()
 
         for i in range(1, amount+1):
