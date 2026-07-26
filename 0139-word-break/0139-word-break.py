7e20a49899a2abe2781dc7b20fa7e7f71493f954
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-07-26 13:38:01 +0900

    Time: 6 ms (25.21%), Space: 19.3 MB (51.67%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -3,12 +3,12 @@ class Solution:
         n = len(s)
         dp = [False] * (n+1)
         dp[0] = True
-
+        wordDict = set(wordDict)
+        
         for i in range(1, n+1):
             for j in range(i):
-                if dp[j] and s[j:i] in set(wordDict):
+                if dp[j] and s[j:i] in wordDict:
                     dp[i] = True
                     break
-
-
-        return dp[n]
+        
+        return dp[-1]
