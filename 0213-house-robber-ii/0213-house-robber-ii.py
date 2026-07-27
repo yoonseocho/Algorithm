8d0ee093627bb5aaceada82250cf63a60c384cf7
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-07-27 23:48:31 +0900

    Time: 0 ms (100%), Space: 19.3 MB (31.49%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -10,32 +10,19 @@ class Solution:
         
         dp1 = [0] * (n+1)
         dp2 = [0] * (n+1)
-        dp3 = [0] * (n+1)
 
-        # case1. 첫집만 터는 경우
-        dp1[1] = nums[0]
-        dp1[2] = nums[0]
-    
-        for i in range(3, n):
-            dp1[i] = max(dp1[i-1], dp1[i-2] + nums[i-1])
+        # case1. 후보에서 첫집 제외
+        dp1[2] = nums[1]
+        dp1[3] = max(nums[1], nums[2])
 
-        dp1[n] = dp1[n-1]
+        for i in range(3, n+1):
+            dp1[i] = max(dp1[i-1], dp1[i-2]+nums[i-1])
 
-        # case2. 끝집만 터는 경우
-        dp2[1] = 0
-        dp2[n] = nums[n-1]
-        dp2[n-1] = dp2[n]
-
-        for i in range(n-2, 1, -1):
-            dp2[i] = max(dp2[i+1], dp2[i+2] + nums[i-1])
-
-        # case3. 첫집 끝집 둘 다 안터는 경우
-        dp3[1] = 0
-        dp3[2] = nums[1]
+        # case2. 후보에서 끝집 제외
+        dp2[1] = nums[0]
+        dp2[2] = max(nums[0], nums[1])
 
         for i in range(3, n):
-            dp3[i] = max(dp3[i-1], dp3[i-2] + nums[i-1])
-
-        dp3[n] = dp3[n-1]
-
-        return max(*dp1, *dp2, *dp3)
+            dp2[i] = max(dp2[i-1], dp2[i-2]+nums[i-1])
+        
+        return max(*dp1, *dp2)
