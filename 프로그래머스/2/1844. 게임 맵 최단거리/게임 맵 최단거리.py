075cd60e87324cd52da5318bae981da333e5ef1e
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-08-01 15:18:31 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.89 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,36 +1,29 @@
 from collections import deque
 
 def solution(maps):
-    q = deque()
-    
     n = len(maps)
     m = len(maps[0])
+    
+    q = deque()
     visited = [[0] * m for _ in range(n)]
     
-    cnt = 0
+    def in_range(x, y):
+        return 0<=x<n and 0<=y<m
+    
+    # bfs
+    q.append((0, 0))
+    visited[0][0] = 1
     
     dxs = [-1, 1, 0, 0]
     dys = [0, 0, -1, 1]
     
-    q.append((0, 0))
-    visited[0][0] = 1
-    
-    def in_range(x, y):
-        return 0<= x < n and 0<= y < m
-    
-    def bfs():
-        nonlocal cnt
-        
-        # if maps[n-1][m-2] == 0 and maps[n-2][m-1] == 0:
-        #     return -1
-        
-        while q:
-            x, y = q.popleft()
+    while q:
+        x, y = q.popleft()
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
             
-            for dx, dy in zip(dxs, dys):
-                nx, ny = x + dx, y + dy
-                if in_range(nx, ny) and not visited[nx][ny] and maps[nx][ny]:
-                    q.append((nx, ny))
-                    visited[nx][ny] = visited[x][y] + 1
-    bfs()
+            if in_range(nx, ny) and not visited[nx][ny] and maps[nx][ny]:
+                visited[nx][ny] = visited[x][y] + 1   
+                q.append((nx, ny))
+    
     return visited[n-1][m-1] if visited[n-1][m-1] else -1
