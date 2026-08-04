42675867ae1b9962aeb9229e4b3d6fbe7c3338bc
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-08-04 12:05:57 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,19 +1,43 @@
--- 코드를 작성해주세요
-SELECT 
-    SUM(G.SCORE) AS SCORE,
+# SELECT
+#     SUM(G.SCORE) AS SCORE,
+#     E.EMP_NO AS EMP_NO,
+#     E.EMP_NAME AS EMP_NAME,
+#     E.POSITION AS POSITION,
+#     E.EMAIL AS EMAIL
+# FROM HR_EMPLOYEES E
+# JOIN HR_GRADE G
+#     ON E.EMP_NO = G.EMP_NO
+# GROUP BY EMP_NO
+# HAVING SUM(G.SCORE) IN (
+#                         SELECT 
+#                             MAX(SCORE)
+#                         FROM (
+#                             SELECT
+#                                SUM(SCORE) AS SCORE
+#                             FROM HR_GRADE
+#                             GROUP BY EMP_NO
+#                         ) T
+#                     )
+
+WITH TMP AS (
+    SELECT
+        EMP_NO,
+        SUM(SCORE) AS SCORE,
+        RANK() OVER (ORDER BY SUM(SCORE) DESC) AS RNK
+    FROM HR_GRADE
+    GROUP BY EMP_NO
+)
+
+SELECT
+    T.SCORE AS SCORE,
     E.EMP_NO AS EMP_NO,
     E.EMP_NAME AS EMP_NAME,
     E.POSITION AS POSITION,
     E.EMAIL AS EMAIL
-FROM HR_DEPARTMENT D
+FROM TMP T
 JOIN HR_EMPLOYEES E
-    ON D.DEPT_ID = E.DEPT_ID
-JOIN HR_GRADE G
-    ON E.EMP_NO = G.EMP_NO
-GROUP BY G.EMP_NO
-HAVING SUM(G.SCORE) = (
-                        SELECT MAX(TOTAL)
-                        FROM (SELECT SUM(SCORE) AS TOTAL
-                            FROM HR_GRADE
-                            GROUP BY EMP_NO) T
-                        )
+    ON T.EMP_NO = E.EMP_NO
+WHERE T.RNK = 1
+
+
+
