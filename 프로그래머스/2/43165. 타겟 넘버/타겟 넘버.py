5da07b240c43d7a2f978be9933e3ddb2fed62bcc
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-08-07 14:47:42 +0900

    [level 2] Title: 타겟 넘버, Time: 105.27 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,19 +1,17 @@
 def solution(numbers, target):
-    n = len(numbers)
     answer = 0
+    n = len(numbers)
     
     def dfs(curr_sum, cnt):
-        # print(f"{"    "*cnt} >>> dfs({curr_sum}, {cnt}) 시작")
         nonlocal answer
         if cnt == n:
             if curr_sum == target:
                 answer += 1
-                # print(f"{"    "*cnt} >>> dfs({curr_sum}, {cnt}) 끝, answer={answer}")
             return
         
-        dfs(curr_sum + numbers[cnt], cnt+1)
-        dfs(curr_sum - numbers[cnt], cnt+1)
-    
+        dfs(curr_sum + numbers[cnt], cnt + 1)
+        dfs(curr_sum - numbers[cnt], cnt + 1)
+
     dfs(0, 0)
     
     return answer
