b54c20614742a0d1820ed148c998ee18d3b11798
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-08-07 19:13:17 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.90 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?gad_source=1&gad_campaignid=23776046558&gbraid=0AAAAAC_c4nBcDVYQRFEx9ax_RlyonH1Rb&gclid=CjwKCAjwhNbTBhB4EiwAsFSg-r8TNNKmnZXNwFZiS2BN-UcP_vxvYqHY87FaL3exkR5Xv1fSlNHvLxoC1tkQAvD_BwE) 
 
 ### 성능 요약
 
-메모리: 11.3 MB, 시간: 8.89 ms
+메모리: 11.1 MB, 시간: 8.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 01일 15:18:29
+2026년 08월 07일 19:13:12
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -3,27 +3,27 @@ from collections import deque
 def solution(maps):
     n = len(maps)
     m = len(maps[0])
-    
     q = deque()
     visited = [[0] * m for _ in range(n)]
     
-    def in_range(x, y):
-        return 0<=x<n and 0<=y<m
-    
-    # bfs
     q.append((0, 0))
     visited[0][0] = 1
     
     dxs = [-1, 1, 0, 0]
     dys = [0, 0, -1, 1]
     
+    def in_range(x, y):
+        return 0<=x<n and 0<=y<m
+    
+    # bfs
     while q:
         x, y = q.popleft()
+        
         for dx, dy in zip(dxs, dys):
             nx, ny = x + dx, y + dy
             
             if in_range(nx, ny) and not visited[nx][ny] and maps[nx][ny]:
-                visited[nx][ny] = visited[x][y] + 1   
+                visited[nx][ny] = visited[x][y] + 1
                 q.append((nx, ny))
     
     return visited[n-1][m-1] if visited[n-1][m-1] else -1
