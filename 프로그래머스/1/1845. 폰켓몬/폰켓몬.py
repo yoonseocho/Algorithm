a2752cf16d45b39e95f3e93c6d99be009adca0ca
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-08-10 15:44:03 +0900

    [level 1] Title: 폰켓몬, Time: 0.58 ms, Memory: 12.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,10 +1,2 @@
 def solution(nums):
-    n = len(nums)
-    can_get = n // 2
-    
-    unique_nums = set(nums)
-    
-    if can_get <= len(unique_nums):
-        return can_get
-    else:
-        return len(unique_nums)
+    return min(len(nums)//2, len(set(nums)))
