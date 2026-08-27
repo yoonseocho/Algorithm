83e801deca82b2b548876a0c22d174e5f3a563f2
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-08-27 21:46:32 +0900

    [level 2] Title: 방문 길이, Time: 0.14 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,24 +1,13 @@
 def solution(dirs):
     answer = set()
-    x, y, nx, ny = 0, 0, 0, 0
-    # d = {"U":(0, 1), "D":(0, -1), "L":(-1, 0), "R":(1, 0)}
+    x, y = 0, 0
+    d = {"U":(0, 1), "D":(0, -1), "L":(-1, 0), "R":(1, 0)}
     
     def in_range(x, y):
         return -5 <= x<= 5 and -5 <= y <= 5
 
     for dir in dirs:
-        if dir == "U":
-            nx = x
-            ny = y + 1
-        elif dir == "D":
-            nx = x
-            ny = y - 1
-        elif dir == "R":
-            nx = x + 1
-            ny = y
-        elif dir == "L":
-            nx = x - 1
-            ny = y
+        nx, ny = x + d[dir][0], y + d[dir][1]
         
         if in_range(nx, ny):
             answer.add((x, y, nx, ny))
