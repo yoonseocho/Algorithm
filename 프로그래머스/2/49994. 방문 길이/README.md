117256789f6512c3e6845439c6e062008ffefd90
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-08-27 21:44:54 +0900

    [level 2] Title: 방문 길이, Time: 0.13 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/README.md
+++ 프로그래머스/2/49994. 방문 길이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 방문 길이 - 49994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994) 
 
 ### 성능 요약
 
-메모리: 11.6 MB, 시간: 0.17 ms
+메모리: 11.7 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 27일 21:41:51
+2026년 08월 27일 21:44:51
 
 ### 문제 설명
 
--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,6 +1,7 @@
 def solution(dirs):
     answer = set()
     x, y, nx, ny = 0, 0, 0, 0
+    # d = {"U":(0, 1), "D":(0, -1), "L":(-1, 0), "R":(1, 0)}
     
     def in_range(x, y):
         return -5 <= x<= 5 and -5 <= y <= 5
@@ -20,8 +21,8 @@ def solution(dirs):
             ny = y
         
         if in_range(nx, ny):
-            if (nx, ny, x, y) not in answer:
-                answer.add((x, y, nx, ny))
+            answer.add((x, y, nx, ny))
+            answer.add((nx, ny, x, y))
             x, y = nx, ny
     
-    return len(answer)
+    return len(answer)//2
