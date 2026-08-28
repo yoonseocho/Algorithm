84bb37da9a089784d162621a3e241ed42b811069
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-08-29 00:27:29 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 33.94 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -4,36 +4,16 @@ def solution(n, k):
     def get_k_num(n, k):
         k_num = []
         
-        while n >= k:
+        while n > 0:
             res = n % k
             k_num.append(res)
 
             n //= k
         
-        k_num.append(n)
         return ''.join(map(str, k_num[::-1]))
         
-    def slice_by_zero(k_num_str):
-        candidates = []
-        k_num_cpy = k_num_str[:]
-        start = 0
-        for i, num in enumerate(k_num_str):
-            if num == "0":
-                slice = k_num_cpy[start:i]
-                if slice != '':
-                    candidates.append(int(slice))
-                
-                start = i+1
-            
-            else:
-                if i == len(k_num_str) -1:
-                    slice = k_num_cpy[start:]
-                    if slice != '':
-                        candidates.append(int(slice))
-        return candidates
-        
     def is_prime(num):
-        if num == 1:
+        if num < 2:
             return False
         
         for div in range(2, int(num**0.5)+1):
@@ -46,7 +26,7 @@ def solution(n, k):
     k_num_str = get_k_num(n, k)
 
     # 소수 찾기
-    candidates = slice_by_zero(k_num_str)
+    candidates = [int(num) for num in k_num_str.split('0') if num != '']
     
     for candidate in candidates:
         if is_prime(candidate):
