e3c1f2387db1cc89433c27b2d9faf07553f66bd9
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-08-29 16:27:26 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.75 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -1,57 +1,26 @@
 import math
-
+from collections import defaultdict
 def solution(fees, records):
-    n = len(records)
-    # records를 입차시간, 차량번호, 입/출 여부 로 나누기
-    times = []
-    car_nums = []
-    in_or_out = []
+    END = 23*60 + 59
+    in_time = {}
+    total_time = defaultdict(int)
+    
     for record in records:
-        split_record = record.split(" ")
-        time = split_record[0].split(":")
-        hour = int(time[0]) * 60
-        minute = int(time[1])
-        times.append(hour+minute)
-        car_nums.append(split_record[1])
-        in_or_out.append(split_record[2])
-    
-    # for time, car_num, in_out in zip(times, car_nums, in_or_out):
-    #     print(time, car_num, in_out)
-    
-    # 누적 주차 시간 계산
-    #car_fee = {5961:320, 0000:321}
-    used_record = [False] * n
-    car_fee = {}
-    for car_num in car_nums:
-        car_fee[car_num] = 0
-    
-    for i in range(n):
-        for j in range(i+1, n):
-            if not used_record[i] and not used_record[j] and car_nums[i] == car_nums[j] and in_or_out[i] == "IN" and in_or_out[j] == "OUT":
-                used_record[i] = True
-                used_record[j] = True
-                car_fee[car_nums[i]] += (times[j] - times[i])
-                break
+        time_str, car_num, status = record.split()
+        h, m = time_str.split(":")
+        time = int(h) * 60 + int(m)
+        
+        if status == "IN":
+            in_time[car_num] = time
         else:
-            if not used_record[i]:
-                used_record[i] = True
-                car_fee[car_nums[i]] += (60*23+59 - times[i]) #23:59
+            total_time[car_num] += time - in_time.pop(car_num)
     
-    # print(car_fee)
+    for car_num, t in in_time.items():
+        total_time[car_num] += END - t
     
-    # 주차 요금 계산
-    final_car_fee = {}
-    for car_num, time in car_fee.items():
-        if time <= fees[0]:
-            final_car_fee[car_num] = fees[1]
-        else:
-            over_time = time - fees[0]
-            fee = fees[1] + math.ceil(over_time / fees[2]) * fees[3]
-            final_car_fee[car_num] = fee
-    # print(final_car_fee)
-    
-    # 차량번호가 작은 차부터 return
-    answer = []
-    for car_num in sorted(list(set(car_nums))):
-        answer.append(final_car_fee[car_num])
-    return answer
+    def calc_fee(t):
+        if t <= fees[0]:
+            return fees[1]
+        return fees[1] + math.ceil((t - fees[0]) / fees[2]) * fees[3]
+  
+    return [calc_fee(total_time[car]) for car in sorted(total_time)]
