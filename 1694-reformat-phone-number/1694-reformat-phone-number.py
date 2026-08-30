6df533e1251f9192ccd5709e11727a86ba1beda9
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-08-30 16:51:18 +0900

    Time: 0 ms (100%), Space: 19.2 MB (67.33%) - LeetHub

--- 1694-reformat-phone-number/1694-reformat-phone-number.py
+++ 1694-reformat-phone-number/1694-reformat-phone-number.py
@@ -1,23 +1,18 @@
 class Solution:
     def reformatNumber(self, number: str) -> str:
-        dash_removed = ''.join(number.split('-'))
-        space_dash_removed = ''.join(dash_removed.split())
+        cleaned = number.replace('-', '').replace(' ', '')
         
-        n = len(space_dash_removed)
-
-        b = n // 3
-        if (n - 3*b) % 2 != 0:
-            b -= 1
-        a = (n - 3*b) // 2
-
+        n = len(cleaned)
+        i = 0
         answer = []
-        last = 0
-        for i in range(0, 3*b, 3):
-            answer.append(space_dash_removed[i:i+3])
-            last = i+3
 
+        while (n - i) > 4:
+            answer.append(cleaned[i:i+3])
+            i += 3
         
-        for i in range(last, last + 2*a, 2):
-            answer.append(space_dash_removed[i:i+2])
-        
+        if len(cleaned[i:]) == 4:
+            answer.append(cleaned[i:i+2])
+            i += 2
+        answer.append(cleaned[i:])
+
         return '-'.join(answer)
