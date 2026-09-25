090c3934f50e79ca8419278bc67aaa54669c28e2
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-09-25 13:22:26 +0900

    [level 2] Title: 타겟 넘버, Time: 117.84 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,16 +1,15 @@
 def solution(numbers, target):
     answer = 0
-    indent = "   "
-    def dfs(curr_sum, depth):
+    def dfs(curr_sum, cnt):
         nonlocal answer
-        if depth == len(numbers):
+        if cnt == len(numbers):
             if curr_sum == target:
                 answer += 1
             return
+                
+        dfs(curr_sum + numbers[cnt], cnt+1)
+        dfs(curr_sum - numbers[cnt], cnt+1)
         
-        dfs(curr_sum + numbers[depth], depth+1)
-        dfs(curr_sum - numbers[depth], depth+1)
-    
     dfs(0, 0)
     
     return answer
