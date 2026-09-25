40c7417c7ef4d14ecab7e5aaaa238f6ccb7caae3
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-09-25 16:39:13 +0900

    Time: 265 ms (21.99%), Space: 21.6 MB (73.86%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,8 +3,6 @@ from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m, n = len(grid), len(grid[0])
-
-        drs, dcs = [-1, 1, 0, 0], [0, 0, -1, 1]
         visited = [[False] * n for _ in range(m)]
 
         def in_range(r, c):
@@ -14,20 +12,24 @@ class Solution:
             q = deque([(r, c)])
             visited[r][c] = True
 
+            drs, dcs = [-1, 1, 0, 0], [0, 0, -1, 1]
+
             while q:
                 r, c = q.popleft()
 
                 for dr, dc in zip(drs, dcs):
-                    nr, nc = r+dr, c+dc
-                    if in_range(nr, nc) and grid[nr][nc] == "1" and not visited[nr][nc]:
+                    nr, nc = r + dr, c + dc
+
+                    if in_range(nr, nc) and not visited[nr][nc] and grid[nr][nc] == "1":
                         q.append((nr, nc))
                         visited[nr][nc] = True
-        
-        cnt = 0
+
+
+        answer = 0
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == "1" and not visited[i][j]:
+                if not visited[i][j] and grid[i][j] == "1":
                     bfs(i, j)
-                    cnt += 1
+                    answer += 1
         
-        return cnt
+        return answer
