4c28a30c3aa76be2bb12ad00cb839f655bc6e43b
yoonseocho <88365583+yoonseocho@users.noreply.github.com>
2026-09-25 13:37:57 +0900

    [level 2] Title: 타겟 넘버, Time: 0.88 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,15 +1,16 @@
+from collections import defaultdict
+
 def solution(numbers, target):
-    answer = 0
-    def dfs(curr_sum, cnt):
-        nonlocal answer
-        if cnt == len(numbers):
-            if curr_sum == target:
-                answer += 1
-            return
-                
-        dfs(curr_sum + numbers[cnt], cnt+1)
-        dfs(curr_sum - numbers[cnt], cnt+1)
-        
-    dfs(0, 0)
+    # dp[i]: 지금까지 처리한 숫자들로 합 i를 만드는 경우의 수
+    dp = {}
+    dp[0] = 1
     
-    return answer
+    for num in numbers:
+        new_dp = defaultdict(int)
+        for s, cnt in dp.items():
+            new_dp[s+num] += cnt
+            new_dp[s-num] += cnt
+        dp = new_dp
+    
+    
+    return dp[target]
